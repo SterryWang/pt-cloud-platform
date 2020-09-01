a277fae6a6856f5bd542c35527384d90da6e9775
wangxg3 <676210623@qq.com>
2020-09-01 18:25:22 +0800

    换用idea开发后重新编译

--- pr-consumer-noreg/src/main/java/com/example/democonsumernoreg/DemoConsumerNoregApplication.java
+++ pr-consumer-noreg/src/main/java/com/example/democonsumernoreg/DemoConsumerNoregApplication.java
@@ -1,7 +1,5 @@
 package com.example.democonsumernoreg;
 
-import static org.hamcrest.CoreMatchers.endsWith;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
